2cf2d34e13c47d572a33c24a4a225c84fa06ed0f
Ayomide <ayomidedans@gmail.com>
2022-06-18 12:36:02 +0100

    misc

--- src/components/Hits.jsx
+++ src/components/Hits.jsx
@@ -4,21 +4,19 @@ import { MdVerifiedUser } from 'react-icons/md';
 
 const Hits = ({ price, name, img, rooms, verified }) => {
   return (
-    <div className='shadow rounded-xl md:rounded-2xl flex flex-col relative'>
+    <div className='shadow rounded-xl md:rounded-2xl flex flex-col relative justify-center'>
       {verified && <MdVerifiedUser className='absolute top-2 right-2' size={20} />}
       <div className="flex mb-2">
         <img src={img} alt="img" className="rounded-2xl w-full h-64" />
       </div>
-      <div className="flex justify-between">
-        <span className="font-manrope text-black">
-          {name}
-        </span>
-        <span className="font-manrope text-black">
-          rooms: {rooms}
-        </span>
-      </div>
+      <span className="font-inter text-black font-medium text-sm">
+        {name}
+      </span>
+      <span className="font-inter text-black font-medium text-xs">
+        Rooms: {rooms}
+      </span>
       <div className="flex">
-        <span className="">
+        <span className="font-inter text-black font-medium">
           ${millify(price)}
         </span>
       </div>
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -8,7 +8,7 @@ const List = ({ isLoading, properties }) => {
   const propsData = properties.hits;
 
   return (
-    <div className='grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 lg:mt-20 md:mt-10 mt-6 lg:px-10 md:mb-10'>
+    <div className='grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:mt-20 md:mt-10 mt-6 lg:px-10 md:mb-10'>
       {isLoading && <Loader />}
       {propsData?.map(item => (
         <Hits key={item.id} price={item.price} name={item.title} img={item.coverPhoto.url} rooms={item.rooms} verified={item.isVerified} />
