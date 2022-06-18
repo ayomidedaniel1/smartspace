91b63eb89d6ecd630cc9f600f49af2dfa0179ac4
Ayomide <ayomidedans@gmail.com>
2022-06-18 12:44:52 +0100

    list design

--- src/components/Hits.jsx
+++ src/components/Hits.jsx
@@ -2,23 +2,30 @@ import millify from 'millify';
 import React from 'react';
 import { MdVerifiedUser } from 'react-icons/md';
 
-const Hits = ({ price, name, img, rooms, verified }) => {
+const Hits = ({ price, name, img, rooms, baths, verified }) => {
   return (
     <div className='shadow rounded-xl md:rounded-2xl flex flex-col relative justify-center'>
       {verified && <MdVerifiedUser className='absolute top-2 right-2' size={20} />}
-      <div className="flex mb-2">
-        <img src={img} alt="img" className="rounded-2xl w-full h-64" />
+      <div className="flex mb-3">
+        <img src={img} alt="img" className="rounded-t-2xl w-full h-64 cursor-pointer" />
       </div>
-      <span className="font-inter text-black font-medium text-sm">
-        {name}
-      </span>
-      <span className="font-inter text-black font-medium text-xs">
-        Rooms: {rooms}
-      </span>
-      <div className="flex">
-        <span className="font-inter text-black font-medium">
-          ${millify(price)}
+      <div className="px-2">
+        <span className="font-inter text-black font-medium text-sm">
+          {name}
         </span>
+        <div className="flex justify-between mt-2 mb-3">
+          <div className="flex flex-row">
+            <span className="font-inter text-black font-medium text-xs pr-3">
+              Rooms: {rooms}
+            </span>
+            <span className="font-inter text-black font-medium text-xs">
+              Baths: {baths}
+            </span>
+          </div>
+          <span className="font-inter text-amber-900 font-medium text-sm">
+            ${millify(price)}
+          </span>
+        </div>
       </div>
     </div>
   );
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -7,11 +7,13 @@ const List = ({ isLoading, properties }) => {
 
   const propsData = properties.hits;
 
+  console.log(propsData);
+
   return (
     <div className='grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:mt-20 md:mt-10 mt-6 lg:px-10 md:mb-10'>
       {isLoading && <Loader />}
       {propsData?.map(item => (
-        <Hits key={item.id} price={item.price} name={item.title} img={item.coverPhoto.url} rooms={item.rooms} verified={item.isVerified} />
+        <Hits key={item.id} price={item.price} name={item.title} img={item.coverPhoto.url} baths={item.baths} rooms={item.rooms} verified={item.isVerified} />
       ))}
     </div>
   );
